bbe38937e175060f664f21464324f03c4f2b1ed6
Marcelo Tadini <tadini.marcelo53@gmail.com>
2023-08-09 19:05:19 -0300

    Adding styles and theme

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles.ts'
+
+function Header(){
+    return(
+        <S.Header>
+            <S.Title>WATTio</S.Title>
+            <S.SpaceHeader>
+                <S.Description>Driving the future of energy</S.Description>
+            </S.SpaceHeader>
+        </S.Header>
+    )
+}
+
+export default Header
